286ddc85d906dc5a1c2c74bb0e4e38930c95aed9
seungmin001 <minnie9808@gmail.com>
2021-08-10 13:57:54 +0900

    컵 맨 위, 액체 맨 위, 맨 아래 가로줄 출력
    잘 보이도록 3줄으로 출력

--- inference.py
+++ inference.py
@@ -23,15 +23,12 @@ def lineDetect(img,seg):
   cArr=auto_canny(sharpness_improve(img))
   Image.fromarray(cArr).save("cArr.jpg")
   
-  for row in cArr.tolist():
-    counter=Counter(row)
-    # print(counter.most_common())
-
   one=[] # seg:1 cup
   two=[] # seg:2 fluid
   # seg에서 1행 당 인식된 부분 저장
   for row in range(len(seg)):
     counter=Counter(seg[row])
+    # cup 인식 부분이 1픽셀 이상 존재하면..
     if counter[1]>0:
       one.append((counter[1],row)) # 한 줄 당 cup 인식 개수, row 위치
     if counter[2]>0:
@@ -46,15 +43,47 @@ def lineDetect(img,seg):
       if x >= int(cup_max*0.1):
         for i in range(len(seg[y])):
           seg[y][i] = 3
+          # 위아래 3줄 칠하기
+          if y == 0:
+            seg[y+1][i]=3
+          elif y == 512:
+            seg[y-1][i]=3
+          else:
+            seg[y+1][i]=3
+            seg[y-1][i]=3
         break
   
     # 물이 인식된 부분 중 컵 최대 길이의 10%이상인 첫 부분을 fluid 시작 부분으로 가정
     if len(two)>0:
       for x,y in two:
         # 10%이상 중 제일 처음 것 기준 seg에 그 줄을 모두 3으로 저장
-        if x >= int(cup_max*0.1):
+        if x >= int(cup_max*0.2):
           for i in range(len(seg[y])):
             seg[y][i] = 3
+            # 위아래 3줄 칠하기
+            if y == 0:
+              seg[y+1][i]=3
+            elif y == 512:
+              seg[y-1][i]=3
+            else:
+              seg[y+1][i]=3
+              seg[y-1][i]=3
+          break
+      
+      # row 내림차순으로 정렬 (물 아랫부분 임시 인식) 
+      sorted_two = sorted(two, key=lambda x: x[1],reverse=True)
+      for x,y in sorted_two:
+        if x >= int(cup_max*0.2):
+          for i in range(len(seg[y])):
+            seg[y][i] = 3
+            # 위아래 3줄 칠하기
+            if y == 0:
+              seg[y+1][i]=3
+            elif y == 512:
+              seg[y-1][i]=3
+            else:
+              seg[y+1][i]=3
+              seg[y-1][i]=3
           break
 
   return seg # TODO : 각 경계선 값 255(seg : 3)인 seg반환

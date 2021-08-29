61153bf047b2e789242e3eb9a95bce21d730ec03
seungmin001 <minnie9808@gmail.com>
2021-08-29 17:45:58 +0900

    추론 시간 측정, trim 함수 적용 및 반환값 재설정

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ import flask
 import werkzeug
 import inference
 from deeplab import DeepLabModel
+import time
+from trim_final import trimLabel
+import os
 
 '''
 # 특정 파일 추론 시
@@ -15,6 +18,7 @@ app = flask.Flask(__name__)
 # 기본 경로 요청 처리
 @app.route('/', methods=['GET','POST'])
 def handle_request():
+    start=time.time()
     imagefile= flask.request.files['image'] # request msg의 image형식 파일
     filename = werkzeug.utils.secure_filename(imagefile.filename) # filename 안전하게 추출
     print("Received image File name : "+ imagefile.filename)
@@ -22,11 +26,19 @@ def handle_request():
 
     # 추론 실행 (segmentation class 배열 반환)
     resized_im,seg_map = inference.run_model(filename, model) 
-
+    print("middle time :",time.time()-start)
     # 경계선 얻어내기
-    seg=inference.lineDetect(resized_im,seg_map)
+    # seg=inference.lineDetect(resized_im,seg_map)
+    issuccess, seg, msg = trimLabel(filename,seg_map)
+    if not issuccess:
+        return {"success":"false","msg":msg}
+    
+    # segmentation 완료 후 저장한 사진 삭제
+    if os.path.exists(filename):
+        os.remove(filename)
 
-    return {"segmap":seg.tolist()} # json data형태로 response msg 보냄.
+    print("processtime : ",time.time()-start)
+    return {"success":"true","segmap":seg.tolist(),"msg":msg} # json data형태로 response msg 보냄.
 
 if __name__ == "__main__":
     model=DeepLabModel('') # 서버 실행 시 모델 미리 로드

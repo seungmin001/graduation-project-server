7dd41cae1ab7d5cfb97bc7c9fc10eb61376951a3
seungmin001 <minnie9808@gmail.com>
2021-09-28 17:01:38 +0900

    json 형식 변경 / IP 임시 제한
    * 데이터 크기 줄이려 전체 배열이 아닌 한계선 위치만 json에 담아 반환
    * 서버로 공격이 들어와 본인의 IP만 접근 허용

--- server.py
+++ server.py
@@ -1,22 +1,27 @@
-import flask
+from flask import Flask, request, abort
 import werkzeug
 import inference
 from deeplab import DeepLabModel
 from trimLabel_210910 import trimLabel,checkAreaOfLiquid, checkVolumnOfLiquid
 import os
-'''
+
 # 특정 파일 추론 시
 import time
 import numpy as np
 from PIL import Image
-'''
 
-app = flask.Flask(__name__)
+
+app = Flask(__name__)
+
+@app.before_request
+def limit_addr():
+    if request.remote_addr != "210.218.145.162":
+        abort(403)
 
 # 기본 경로 요청 처리
 @app.route('/', methods=['POST'])
 def handle_request():
-    imagefile= flask.request.files['image'] # request msg의 image형식 파일
+    imagefile= request.files['image'] # request msg의 image형식 파일
     filename = werkzeug.utils.secure_filename(imagefile.filename) # filename 안전하게 추출
     print("Received image File name : "+ imagefile.filename)
     imagefile.save(filename) # image file로 save
@@ -31,14 +36,16 @@ def handle_request():
         return {"success":"false","msg":msg}
     
     # check fluid
-    requiredRatio = flask.request.form["ratio"]
+    ingName = request.form["ingName"]
+    requiredRatio = request.form["ratio"]
     print("requiredRatio",requiredRatio)
     # isCheckable : ratio 측정 성공여부 및 넘김 여부 , ratioMsg : 해당 결과 설명문
-    _, isCheckable, ratioMsg, ratioStatus = checkVolumnOfLiquid(seg,float(requiredRatio))
+    lineLoc, isCheckable, ratioMsg, ratioStatus = checkVolumnOfLiquid(seg,float(requiredRatio))
+    
     if isCheckable:
-        return {"success":"true","segmap":seg.tolist(),"msg":msg, "ratio":"true", "ratioMsg":ratioMsg,"ratioStatus":ratioStatus}
+        return {"ingName":ingName,"success":"true","segmap":lineLoc,"msg":msg, "ratio":"true", "ratioMsg":ratioMsg,"ratioStatus":ratioStatus}
     else :
-        return {"success":"true","segmap":seg.tolist(),"msg":msg, "ratio":"false", "ratioMsg":ratioMsg,"ratioStatus":ratioStatus}
+        return {"ingName":ingName,"success":"true","segmap":lineLoc,"msg":msg, "ratio":"false", "ratioMsg":ratioMsg,"ratioStatus":ratioStatus}
     # segmentation 완료 후 저장한 사진 삭제
     if os.path.exists(filename):
         os.remove(filename)
@@ -52,24 +59,24 @@ if __name__ == "__main__":
     
     '''
     # 특정 파일 추론 시
-    time.sleep(5)
-
-    _,seg_map = inference.run_model("./savedModel/image/glass_10263.jpg", model)
-    print(len(seg_map), len(seg_map[0]))
-
-    # file=open("./maptxt/glass_30196.txt","w")
-    # np.savetxt(file, seg_map.astype(int), fmt='%i')
-    # file.close()
 
     # Make LUT (Look Up Table) with your 3 colours
-    LUT = np.zeros((3,3),dtype=np.uint8)
+    LUT = np.zeros((4,3),dtype=np.uint8)
     LUT[0]=[0,0,0]
     LUT[1]=[0,255,0]
     LUT[2]=[0,0,255]
-    pixelmap=LUT[seg_map]
+    LUT[3]=[255,0,0]  
 
-    im = Image.fromarray(pixelmap)
-    im.save("./savedModel/image/your_array10263.jpg")
+    # 추론
+    for i  in range(19,21) :
+        filename="./savedModel/air/"+str(i)+".jpg"
+        _,seg_map = inference.run_model(filename, model)
+        issuccess, seg, msg = trimLabel(filename,seg_map)
+        lineLoc, isCheckable, ratioMsg, ratioStatus = checkVolumnOfLiquid(seg,float(100))
+        print(msg,ratioMsg)
+        pixelmap=LUT[seg_map] # label to color mapping
+        im = Image.fromarray(pixelmap)
+        im.save("./savedModel/air/"+str(i)+"_22_trim.jpg")
     #file=open("./maptxt/glass_30196.txt","w")
     #np.savetxt(file, seg_map.astype(int), fmt='%i')
     #file.close()
